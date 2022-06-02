7af1d9471fb2fb2222690b4abeaea60316ef17d6
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2022-06-02 12:56:09 +0200

    Javadoc documentazione

--- Documentazione/package-search-index.js
+++ Documentazione/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"MyTest"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"myAdapter"},{"l":"MyAdapter"}];updateSearchResults();
